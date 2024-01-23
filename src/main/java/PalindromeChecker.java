faeb1ddff38455c39d19a4b282b6db2420c50eef
koa-s <143123833+koa-s@users.noreply.github.com>
2024-01-23 11:09:12 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -35,15 +35,55 @@ public void tester()
     }
   }
 }
-public boolean palindrome(String word)
+public void setup()
 {
-  //your code here
+  String lines[] = {"test", "rotator", "rewriter", "nurses run", "Madam, I'm Adam!", "A Man! A Plan! A Canal! Panama!", };
+  System.out.println("there are " + lines.length + " lines");
+  for (int i=0; i < lines.length; i++) 
+  {
+    if (palindrome(lines[i])==true)
+    {
+      System.out.println(lines[i] + " IS a palindrome.");
+    } else
+    {
+      System.out.println(lines[i] + " is NOT a palindrome.");
+    }
+  }
+}
+public boolean palindrome(String sString)
+{
+  String str="";
+  String word = noCapitals(onlyLetters(sString));
+  for (int i = word.length(); i>0; i--)
+  {
+    str=str+word.substring(i-1, i);
+  }
+  if (str.equals(word))
+  {
+    return true;
+  }
   return false;
 }
 public String reverse(String str)
 {
-    String sNew = new String();
-    //your code here
-    return sNew;
+  String sNew = new String();
+  for (int i = str.length(); i>0; i--)
+  {
+    sNew=sNew+str.substring(i-1, i);
+  }
+  return sNew;
 }
+public String onlyLetters(String sString) {
+  String str = "";
+  for (int i = 0; i<sString.length(); i++)
+  {
+    if (Character.isLetter(sString.charAt(i)))
+    {
+      str = str+sString.substring(i, i+1);
+    }
+  }
+  return str;
+}
+public String noCapitals(String sWord) {
+  return sWord.toLowerCase();
 }
